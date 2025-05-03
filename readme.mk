a783e1969f2f74d9e3156e87cf5bf133e73ca663
Felipe Magalhaes <31110785+felipe-magalhaes-DevCorner@users.noreply.github.com>
2025-05-03 18:16:35 -0300

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -1,7 +1,3 @@
-1 - Script de execução ja presente na ferramenta.
-
-
-Ao clicar em start ja deve funcionar perfeitamente, server e cliente.
-
-
-2 - Testes unitarios podem ser rodados diretamente pelo TestExploerer sem nenhuma nescessidade de alteração.
+Abrir a soluÃ§Ã£o no visual studio
+Ja existe um script de execuÃ§Ã£o na ferramenta. Ao clicar start deve tudo se resolver.
+Testes unitarios podem ser rodados diretamente pelo TestExploerer sem nenhuma nescessidade de alteraÃ§Ã£o.
